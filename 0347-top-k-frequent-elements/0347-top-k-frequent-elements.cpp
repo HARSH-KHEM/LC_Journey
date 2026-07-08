4a0174a2114f62f61ee7921e142a7bd43433f269
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-07-08 18:41:59 +0530

    Time: 4 ms (33.82%), Space: 17.9 MB (62.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size() ;
-        vector<pair<int,int>>freq ;
-        for(int i = 0 ; i<n ; i++){
-            bool ap = false ;
-            for(auto p : freq){
-                if(p.first == nums[i]){
-                    ap = true ;
-                    break ;
-                }
-            }
-            if(ap)continue ;
-            int cnt = 0 ;
-            for(int j = 0 ; j<n ; j++){
-                if(nums[j]==nums[i]){
-                    cnt++ ;
-                }
-            }
-            freq.push_back({nums[i],cnt});
+        unordered_map<int,int>mp ;
+        for(auto x : nums){
+            mp[x]++;
         }
-
+        vector<pair<int,int>>freq ;
+        for(auto &p : mp){
+            freq.push_back({p.first,p.second});
+        }
         vector<int>ans ;
         while(k--){
-        int idx = 0 ;
-        for( int i = 1 ; i<freq.size() ; i++){
-         if(freq[i].second>freq[idx].second) idx = i ;
-        }
-         ans.push_back(freq[idx].first);
-         freq[idx].second=-1 ;
+            int idx = 0 ;
+            for( int i = 1 ; i<freq.size() ; i++){
+                if(freq[i].second > freq[idx].second)idx = i ;
+            }
+            ans.push_back(freq[idx].first);
+            freq[idx].second = -1 ;
         }
         return ans ;
     }
