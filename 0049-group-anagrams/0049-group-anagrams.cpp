8bc1583477b76a3be3407d17baf3e04fd4bb34fb
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-07-09 01:24:59 +0530

    Time: 77 ms (6.39%), Space: 34.5 MB (8.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,9 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>mp;
         for(auto s : strs){
-          string temp = s ;
-          sort(temp.begin(),temp.end());
-          mp[temp].push_back(s);
+          vector<int>freq(26,0);
+          for( char c : s){
+            freq[c-'a']++;
+          }
+          string key ="";
+          for(int x : freq){
+            key+= to_string(x)+'#';
+          }
+          mp[key].push_back(s);
         }
           vector<vector<string>>ans;
           for(auto &it : mp){
