0f1e824aa42dc102efcb6e79d339a505dc300c5a
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-07-11 07:58:42 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.08%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,9 +5,9 @@ public:
         int l = 0 , r = n-1 ;
         while(l<=r){
             int mid = l + (r-l)/2 ;
-            if(nums[mid]>target)r=mid-1;
+            if(nums[mid]>=target)r=mid-1;
             else if(nums[mid]<target)l=mid+1;
-            else return mid ;
+           
         }
         return l ;
     }
