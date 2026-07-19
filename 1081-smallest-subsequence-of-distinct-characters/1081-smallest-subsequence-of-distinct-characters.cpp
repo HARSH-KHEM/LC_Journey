dd1a556a977436ee8251bd0886a56475a64c6b70
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-07-19 09:50:41 +0530

    Time: 0 ms (100%), Space: 9.1 MB (9.27%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-
-        vector<int> last(26);
-        vector<bool> inStack(26, false);
-
-
-        for (int i = 0; i < s.size(); i++)
-            last[s[i] - 'a'] = i;
-
-        stack<char> st;
-
-        for (int i = 0; i < s.size(); i++) {
-
-            char c = s[i];
-
-
-            if (inStack[c - 'a'])
-                continue;
-
-            while (!st.empty() &&
-                   st.top() > c &&
-                   last[st.top() - 'a'] > i) {
-
-                inStack[st.top() - 'a'] = false;
-                st.pop();
-            }
-
-            st.push(c);
-            inStack[c - 'a'] = true;
+        vector<int>last(26);
+        vector<bool>instack(26,false);
+        for( int i = 0 ; i<s.size() ; i++){
+            last[s[i]-'a']=i;
         }
-
-        string ans;
-
-        while (!st.empty()) {
-            ans += st.top();
+        stack<int>st ;
+        for( int i = 0 ; i<s.size() ; i++){
+            char c = s[i];
+            if(instack[c-'a'])continue ;
+            while(!st.empty() && st.top()> c && last[st.top()-'a']>i){
+                instack[st.top() - 'a']=false;
+            st.pop();
+            }
+            st.push(c);
+            instack[c-'a']=true ;
+        }
+        string ans ;
+        while(!st.empty()){
+            ans+=st.top();
             st.pop();
         }
-
-        reverse(ans.begin(), ans.end());
-
-        return ans;
+        reverse(ans.begin(),ans.end());
+        return ans ;
     }
 };
