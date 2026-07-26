190ab9090de6059f5fe3b54bd5c34fe55027b675
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-07-26 13:51:44 +0530

    Time: 0 ms (100%), Space: 18.6 MB (39.37%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
+    vector<bool>vis ;
+    vector<bool>recpath ;
+    vector<int>topo ;
+    vector<vector<int>>adj ;
+    bool dfs(int node){
+        vis[node]= true ;
+        recpath[node] = true ;
+        for(auto neigh : adj[node]){
+          if(!vis[neigh]){
+            if(dfs(neigh))return true ;
+          }
+          else if(recpath[neigh]) return true ;
+        }
+        recpath[node]= false ;
+        topo.push_back(node);
+        return false ;
+    }
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        vector<vector<int>>adj(n);
-        vector<int>degree(n,0);
+        adj.assign(n,{});
+        vis.assign(n,false);
+        recpath.assign(n,false);
         for(auto &edge : pre){
            int u = edge[0];
            int v = edge[1];
            adj[v].push_back(u);
-           degree[u]++;
         }
-        queue<int>q ;
-        for( int i = 0 ;i<n ; i++){
-            if(degree[i]==0)q.push(i);
+       for( int i = 0 ; i<n ; i++){
+        if(!vis[i]){
+            if(dfs(i))return {};
         }
-        int cnt = 0 ;
-        vector<int>topo ;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop() ;
-            topo.push_back(node);
-            cnt++ ;
-            for( auto neigh : adj[node]){
-                degree[neigh]--;
-                if(degree[neigh]==0)q.push(neigh);
-            }
-        }
-            if(cnt<n)return {};
-            return topo ;
+       }
+       reverse(topo.begin(),topo.end());
+       return topo ;
     }
 };
