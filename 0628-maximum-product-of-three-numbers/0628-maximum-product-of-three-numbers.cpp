d790386e00a721d09bd15cb385cdee0a838f518d
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-07-26 13:16:29 +0530

    Time: 0 ms (100%), Space: 31.5 MB (76.45%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int max1 = INT_MIN , max2 = INT_MIN , max3= INT_MIN , min1 = INT_MAX , min2 = INT_MAX;
+        for(auto n : nums){
+          if(max1<=n){
+           max3 = max2 ;
+           max2 = max1 ;
+           max1 = n;
+          }
+          else if (max2<=n){
+            max3 = max2 ;
+            max2 = n;
+          }else if(max3<=n){
+            max3 = n ;
+          }
+          if(n<=min1){
+            min2 = min1 ;
+            min1 = n ;
+          }
+          else if(n<=min2){
+            min2 = n ;
+          }
+        }
+        return max(max1*max2*max3 , max1*min1*min2);
+    }
+};
