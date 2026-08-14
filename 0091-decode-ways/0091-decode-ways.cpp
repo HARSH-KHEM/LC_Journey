da72fe91670d49f4a8641df61d64e4b100ea4b60
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-08-14 13:22:35 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.57%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    // TOP - DOWN APPROACH 
-    vector<int>dp;
-    int solve( int i , string &s){
-        int n = s.size() ;
-        if(i==n) return 1 ;
-        if(s[i]=='0')return 0 ;
-        if(dp[i]!=-1)return dp[i];
-        int ways = solve(i+1,s);
-        if(i+1<n){
-            int num = (s[i]-'0')*10 + (s[i+1]-'0');
-            if(num>=10 && num<=26)ways+=solve(i+2,s);
-        }
-        return dp[i]=ways ;
-    }
     int numDecodings(string s) {
         int n = s.size() ;
-        dp.assign(n,-1);
-        return solve(0,s);
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+        for( int i = n-1 ; i>=0 ; i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+                continue ;
+            }
+            dp[i]=dp[i+1];
+            if(i+1<n){
+                int num = (s[i]-'0')*10 + (s[i+1]-'0');
+                if(num>=10 && num<=26){
+                    dp[i]+=dp[i+2];
+                }
+            }
+        }
+        return dp[0];
     }
 };
