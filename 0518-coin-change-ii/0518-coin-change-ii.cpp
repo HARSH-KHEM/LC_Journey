afc4e47ea19af7594b891b91e6153b04b0fd10f6
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-08-24 20:42:28 +0530

    Time: 47 ms (24.92%), Space: 36.8 MB (33.84%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+    vector<vector<int>>dp ;
+    int solve(int indx , int amt , vector<int>&coins){
+        if(amt==0)return 1 ;
+        if(indx == coins.size())return 0 ;
+        if(dp[indx][amt]!=-1)return dp[indx][amt];
+        int take = 0;
+        if(coins[indx]<=amt){
+            take += solve(indx , amt - coins[indx],coins);}
+        int skip = solve(indx+1 , amt , coins);
+        return dp[indx][amt]=take + skip ;
+    }
     int change(int amount, vector<int>& coins) {
-        vector<int>dp(amount+1,0);
-        dp[0]=1 ;
-        for(auto x:coins){
-            for( int amt = x ; amt<=amount ;amt++){            
-                    dp[amt]+=(long long)dp[amt-x];               
-            }
-        }
-
-        return (int)dp[amount];
+        int n = coins.size() ;
+        dp.assign(n,vector<int>(amount+1,-1));
+        return solve(0,amount,coins);
     }
 };
