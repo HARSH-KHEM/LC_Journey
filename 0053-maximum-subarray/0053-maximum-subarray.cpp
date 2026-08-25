d400a6e55f7daada1cfd007dab1f74d3a974a580
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-08-25 20:43:56 +0530

    Time: 0 ms (100%), Space: 74.5 MB (14.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int ans = INT_MIN ; 
-    vector<int>dp ;
-    int solve( int idx , vector<int>&nums){
-        if(idx==0){
-        ans = max(ans,nums[0]);
-        return dp[0] = nums[0];}
-        if(dp[idx]!=-1)return dp[idx];
-        int previous = solve(idx-1,nums);
-        int curr = max(nums[idx],nums[idx]+previous);
-        ans = max(ans,curr);
-        return dp[idx] = curr ;
-    }
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size() ;
-        dp.assign(n,-1);
-        solve(n-1,nums);
-        return ans ;
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        dp[0]=nums[0];
+        int ans = dp[0] ;
+        for( int i = 1 ; i<n ; i++){
+             dp[i]=max(nums[i],nums[i]+dp[i-1]);
+             ans=max(ans,dp[i]);
+        }
+     return ans ;
     }
 };
