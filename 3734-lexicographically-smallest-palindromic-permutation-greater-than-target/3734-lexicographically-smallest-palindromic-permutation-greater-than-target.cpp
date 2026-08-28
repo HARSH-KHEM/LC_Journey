d38121e0e7de89b7172866f30a1b903a5ecae0bb
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-08-28 22:20:07 +0530

    Time: 3 ms (70.77%), Space: 10.5 MB (89.23%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -1,94 +1,72 @@
 class Solution {
 public:
-    string isPossible(int n, vector<int> freq, string cur, char &mid, string& target){
-        // this gives the max palindrome achievable with the current prefix 
-        //(descending order)
-        for(int i=25; i>=0; i--){
-            while(freq[i]){
-                cur += (char)('a'+i);
-                freq[i]--;
-            }
-        }
-        if(mid!='#'){
-            // odd-length palindrome: left half + mid + reverse(left half)
-            string temp = cur;
-            cur += mid;
-            reverse(temp.begin(), temp.end());
-            cur.append(temp.begin(), temp.end());
-        }
-        else {
-            // even-length palindrome: left half + reverse(left half)
-            string temp = cur;
-            reverse(temp.begin(), temp.end());
-            cur.append(temp.begin(), temp.end());
-        }
-        // feasibility check: only valid if this (largest possible) candidate beats target
-        return cur>target? cur : "";
-    }
-
     string lexPalindromicPermutation(string s, string target) {
-        int n = s.size();
-        vector<int> freq(26, 0);
-        if(n==1){
-            // if size is 1 then direct compare
-            if(s>target) return s;
-            else return "";
+        int n=s.size();
+        vector<int>hsh(26,0);
+        for(int i=0;i<n;i++){
+            hsh[s[i]-'a']++;
         }
-        for(char c : s) freq[c-'a']++; // freq Track
-
-        char mid = '#';
-        int oddCount = 0;
-
-        for(int i=0; i<26; i++){
-            if(freq[i]%2){
-                // odd count -> this becomes the middle character
-                mid = (char)('a'+i);
-                freq[i]--;
-                oddCount++;
+        string ans=s;
+        int odd=0;
+        for(int i=0;i<26;i++){
+            if(hsh[i]%2!=0){
+                odd++;
+                ans[n/2]=char('a'+i);
             }
-
-            freq[i] /= 2; // each char used freq[i]/2 times in the left half
-
-            if(oddCount>=2) return ""; // more than one odd-frequency char -> can't form a palindrome
+            hsh[i]/=2;
         }
-
-        n /= 2; // we only need to construct the left half now
-        string res = "", prefix = "";
-
-        // greedily build the left half, position by position
-        for(int i=0; i<n; i++){
-
-            string cur = prefix;
-            bool isThereAny = false;
-
-            // try smallest character first ('a' -> 'z')
-            for(int j=0; j<26; j++){
-                if(freq[j]){
-                    freq[j]--;
-                    cur += (char)('a'+j);
-
-                    // check if this prefix can still lead to a palindrome > target
-                    string isPos = isPossible(n, freq, cur, mid, target);
-
-                    if(isPos!=""){
-                        prefix = cur;      // keep this character, lock in the prefix
-                        isThereAny = true;
-
-                        if(res=="") res = isPos;
-                        else res = min(res, isPos); // track smallest valid candidate seen
-                        break;
-                    }
-
-                    // this character doesn't work, undo and try the next one
-                    freq[j]++;
-                    cur.pop_back();
+        if(odd>1){
+            return "";
+        }
+        int p=0;
+        while(p<n/2 && hsh[target[p]-'a']){
+            hsh[target[p]-'a']--;
+            p++;
+        }
+        int start=min(n-1,p);
+        for(int i=start;i>=0;i--){
+            if(i==(n/2)){
+                for(int j=0;j<n/2;j++){
+                    ans[j] = target[j];
+                    ans[n-1-j] = target[j];
+                }
+                if(ans > target){
+                    return ans;
+                }
+                continue;
+            }
+            int b=target[i]-'a';
+            if(i<p){
+                hsh[b]++;
+            }
+            int idx=-1;
+            for(int c=b+1;c<26;c++){
+                if(hsh[c]){
+                    hsh[c]--;
+                    idx=c;
+                    break;
                 }
             }
-
-            if(!isThereAny)
-                return ""; // no character works at this position -> impossible
+            if(idx==-1){
+                continue;
+            }
+            for(int j=0;j<i;j++){
+                ans[j]=target[j];
+            }
+            ans[i]=char('a'+idx);
+            int k=i+1;
+            for(int c=0;c<26;c++){
+                while(k<n && hsh[c]){
+                    ans[k]=char(c+'a');
+                    k++;
+                    hsh[c]--;
+                }
+            }
+            for(int j=0;j<n/2;j++){
+                ans[n-1-j]=ans[j];
+            }
+            return ans;
         }
-
-        return res; 
+        return "";
     }
 };
