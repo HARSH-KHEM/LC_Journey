fd642414f5b604f7f5e4cc34b044750f40ba105a
Harsh Khem <harshkhem_se24b02_006@dtu.ac.in>
2026-09-08 13:27:46 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+       long long ans  = 0 ;
+        long long base = 1000 ;
+        while(base<=n){
+            ans+=(n-base+1);
+            base*=1000;
+        }
+        return ans ;
+    }
+};
